79886615ab828b4ee6177a60e5512acd71c11836
Tao Wang <hellotaotao@gmail.com>
2025-06-01 00:58:33 +0930

    Initial Commit

---
+++ WhisperNotes/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  WhisperNotes
+//
+//  Created by Tao Wang on 1/6/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ WhisperNotesTests/WhisperNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperNotesTests.swift
+//  WhisperNotesTests
+//
+//  Created by Tao Wang on 1/6/2025.
+//
+
+import Testing
+@testable import WhisperNotes
+
+struct WhisperNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
